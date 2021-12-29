3d6e32741e93ea5b97a10362826e7b2d1264090a
Noam Shani <Noams156@gmail.com>
2021-12-29 20:00:48 +0100

    latest solutions

--- index.js
+++ index.js
@@ -142,3 +142,20 @@ function twoOldestAges(ages){
       return sortedA.slice(-2)
   
   }
+
+
+  // 6 kyuCount characters in your string
+
+
+  function count (string) {  
+
+    const count = {};
+          
+    string.split('').forEach(char => {
+      
+      count[char] = count [char] ? (count[char] +1) :1;
+      
+      
+    })
+    return count
+  }
