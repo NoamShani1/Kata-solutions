104e745a77f83a55795bcc924dcdd2400227c1d4
Noam Shani <Noams156@gmail.com>
2022-01-12 17:10:39 +0100

    latest solutions

--- index.js
+++ index.js
@@ -189,3 +189,20 @@ function twoOldestAges(ages){
 
     
 
+  // 6 kyu Which are in?
+  function inArray(array1,array2){
+    var x;
+    var y;
+    var r = [];
+    for (x = 0; x < array1.length; x++) { 
+      for (y = 0; y < array2.length; y++) {
+        if(array2[y].includes(array1[x]) === true){
+            r.push(array1[x]);
+        }else{
+  
+        }
+      }
+    }
+    r = [...new Set(r)];
+    return r.sort();
+    }
