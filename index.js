3112bc90fc71a4c62560d72f92c087194f32882e
Noam Shani <Noams156@gmail.com>
2022-04-04 20:53:36 +0200

    latest sol

--- index.js
+++ index.js
@@ -384,3 +384,19 @@ function nextId(ids){
     console.log(newStr);
     return newStr;
 }
+
+
+function accum(s) {
+  const result = []
+  const lowerStr = s.toLowerCase()
+
+  for(let i = 0; i < lowerStr.length; i++) {
+    let str = lowerStr[i].toUpperCase()
+    for(let j = 0; j < i; j++) {
+      str += lowerStr[i]
+    }
+    result.push(str)
+  }
+
+  return result.join("-")
+}
